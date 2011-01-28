1086fbf2209a2cf0d61ea4afe5b994e56b4cbdea
Antti <refresh.xss@gmail.com>
2011-01-28 15:41:14 +0200

    Don't use admin scope.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 UkVisa::Application.routes.draw do
   visa_types = [:visa_applications,:uk_visa_applications,:dk_visa_applications]
   visa_types.each do |r|
-    resources r, :controller => "visa_applications", :only => ['index','show']
+    resources r, :controller => "visa_applications" do#, :only => ['index','show']
+      member do
+        get 'update_status'
+      end
+    end
   end
-
+=begin
   scope "/admin" do
      visa_types.each do |r|
        resources r, :controller => "visa_applications" do
@@ -13,6 +17,6 @@ UkVisa::Application.routes.draw do
        end
      end
   end
-  
+=end
   root :to => 'visa_applications#index'
 end
