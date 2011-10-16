82edd55fb6088268e42e753f739c304d4db7ebf9
Andriy Dmytrenko <refresh.xss@gmail.com>
2011-10-16 15:09:35 +0100

    More..

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,21 +7,24 @@
 
 Visa = (function(){
   return {
+    init: function(){
+      $("a.update_visa_status").click(function(){
+        Visa.update_status($(this).parents("tr").attr("data-id"));
+        return false;
+      });
+    },
     update_status: function(id){
-      $("tr[data-id="+id+"] td.visa_status").append("<img class='spinner' src='/assets/loading-spinner.gif' alt='spinner' />");
-      $.get($("tr[data-id="+id+"] a.update_visa_status").attr("href"),function(visa_application){
-        $("tr[data-id="+id+"] span.status").text(visa_application.status);
-        $("tr[data-id="+id+"] span.notes").text("Updated at "+visa_application.updated_at)
-        $("tr[data-id="+id+"] td.visa_status").find('img.spinner').remove();
+      var row = $("tr[data-id="+id+"]");
+      row.find("td.visa_status").append("<img class='spinner' src='/assets/loading-spinner.gif' alt='spinner' />");
+      $.get(row.find("a.update_visa_status").attr("href"),function(visa_application){
+        row.find("span.status").text(visa_application.status);
+        row.find("span.notes").text("Updated at "+visa_application.updated_at)
+        row.find("td.visa_status").find('img.spinner').remove();
       });
     }
   }
 })();
 
-
 $().ready(function(){
-  $("a.update_visa_status").click(function(){
-    Visa.update_status($(this).parents("tr").attr("data-id"));
-    return false;
-  });
+  Visa.init();
 });
