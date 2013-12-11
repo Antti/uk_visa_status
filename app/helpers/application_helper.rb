3adaafc6e7bafa6292e36bb7fa113b598789370b
Andrii Dmytrenko <andrey@reevoo.com>
2013-12-11 22:27:03 +0000

    Some more styling. Add navbar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def flag_img(country_code)
     image_tag "flags/#{country_code}.png", :class => "flag-icon", :alt => country_code
   end
+
+  def can_edit?
+    !!current_user
+  end
 end
