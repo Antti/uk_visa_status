89f1648f94bd932de9d03549a20919966639193a
Andrii Dmytrenko <andrey@reevoo.com>
2013-12-15 00:03:18 +0000

    Return from mailer if there are no changes.

--- app/mailers/visa_application_status_mailer.rb
+++ app/mailers/visa_application_status_mailer.rb
@@ -2,6 +2,7 @@ class VisaApplicationStatusMailer < ActionMailer::Base
   default :from => "Andriy's Dmytrenko <andrey@reevoo.com>"
 
   def status_updated(visa_application, changes, options={})
+    return if changes.empty?
     @visa_application = visa_application
     @changes = changes
     options[:to] = visa_application.notify_email if visa_application.notify_email.present?
